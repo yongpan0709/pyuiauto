35c560cef6e2d08b201381154950b54e11ead65f
huayp <yongpanhua@163.com>
2022-11-17 17:33:45 +0800

    use pyautogui to start app

--- demo.py
+++ demo.py
@@ -1,4 +1,4 @@
 import pywinauto
 from pywinauto.application import Application
 
-app = Application(backend='uia').start('C:\\Users\yongpan.hua\AppData\Local\Feishu\Feishu.exe')
+app = Application(backend='uia').start('C:\\')
---
+++ start_vally.py
@@ -0,0 +1,10 @@
+import pyautogui as gui
+import time
+
+gui.doubleclick(40, 740)
+time.sleep(10)
+gui.click(1239, 475)
+time.sleep(10)
+gui.click(1221, 485)
+time.sleep(10)
+gui.click(1242, 674)
